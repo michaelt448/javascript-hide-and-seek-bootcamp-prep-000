c842e1f54676bd27340c9b8b83cfefe2466625a0
michael <michaeltilman77@gmail.com>
2018-06-12 22:22:00 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,19 @@
+function getFirstSelector(selector){
+ return document.querySelector(selector)
+}
+function nestedTarget(){
+ return document.getElementById('nested')
+}
+function increaseRankBy(n){
+  const list= document.querySelectorAll('.ranked-list')
+  for(let i = 0; i<list.length; i++ ){
+    list[i].innerHTML = (i + n).toString()
+  }
+}
+function deepestChild(){
+  const list = document.querySelectorAll("#grand-node")
+  for(let i = 0; i<list.length; i++){
+    if(document.querySelector('div').innerHTML)
+    }
+  
+}
